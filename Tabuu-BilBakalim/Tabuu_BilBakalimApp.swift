e017c888a0f6696f27f7224104e398efe8eeaf90
abaspnr <117168519+abaspnr@users.noreply.github.com>
2026-01-11 20:19:02 +0300

    Initial Commit

---
+++ Tabuu-BilBakalim/Tabuu_BilBakalimApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabuu_BilBakalimApp.swift
+//  Tabuu-BilBakalim
+//
+//  Created by Abdullah B on 11.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Tabuu_BilBakalimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
